58dfb0cdebdcf85a274b9711f4b7f763b0cf2b8f
wazeer zulfikar <wazeer.zulfikar@gmail.com>
2019-01-29 19:50:43 -0500

    Continue training from previous model function

--- train.py
+++ train.py
@@ -1,16 +1,21 @@
 from keras.layers import Lambda
 from models import *
 from losses import *
+import os
 
-image_size = 32
-discriminator_A = discriminator(image_size)
-discriminator_B = discriminator(image_size)
+image_size = 128
 
-generator_A2B, real_A, fake_B = resnet_generator(image_size)
-generator_B2A, real_B, fake_A = resnet_generator(image_size)
+load_from = 50
+load_path = '/saved_models'
+
+discriminator_A = discriminator(image_size, load_path=os.path.join(load_path, 'discriminator_A_{}.h5'.format(load_from)))
+discriminator_B = discriminator(image_size, load_path=os.path.join(load_path, 'discriminator_B_{}.h5'.format(load_from)))
+
+generator_A2B, real_A, fake_B = resnet_generator(image_size, load_path=os.path.join(load_path, 'generator_A2B_{}.h5'.format(load_from)))
+generator_B2A, real_B, fake_A = resnet_generator(image_size, load_path=os.path.join(load_path, 'generator_B2A_{}.h5'.format(load_from)))
 
 print(discriminator_A.summary())
-print('------------------------------------------------------------------------------------------------------------')
+print('-'*150)
 print(generator_A2B.summary())
 
 recon_B = generator_A2B(fake_A)
@@ -30,8 +35,8 @@ for l in discriminator_A.layers:
 for l in discriminator_B.layers:
 	l.trainable=False
 
-generator_train_function = Model(inputs=[real_A, real_B],outputs=Lambda(G_loss)(lambda_layer_inputs))
-generator_train_function.compile('adam', loss='mae')
+generator_trainer = Model(inputs=[real_A, real_B],outputs=Lambda(G_loss)(lambda_layer_inputs))
+generator_trainer.compile('adam', loss='mae')
 
 
 discriminator_A_real = discriminator_A(real_A)
@@ -49,8 +54,8 @@ for l in discriminator_A.layers:
 for l in discriminator_B.layers:
 	l.trainable=False
 
-discriminator_A_train_function = Model(inputs=[real_A, fake_A_input], outputs=Lambda(D_loss)(lambda_layer_inputs))
-discriminator_A_train_function.compile('adam', loss='mae')
+discriminator_A_trainer = Model(inputs=[real_A, fake_A_input], outputs=Lambda(D_loss)(lambda_layer_inputs))
+discriminator_A_trainer.compile('adam', loss='mae')
 
 
 discriminator_B_real = discriminator_B(real_B)
@@ -68,11 +73,12 @@ for l in discriminator_A.layers:
 for l in discriminator_B.layers:
 	l.trainable=True
 
-discriminator_B_train_function = Model(inputs=[real_B, fake_B_input], outputs=Lambda(D_loss)(lambda_layer_inputs))
-discriminator_B_train_function.compile('adam', loss='mae')
+discriminator_B_trainer = Model(inputs=[real_B, fake_B_input], outputs=Lambda(D_loss)(lambda_layer_inputs))
+discriminator_B_trainer.compile('adam', loss='mae')
 
-
-
+trainers = [generator_trainer, discriminator_A_trainer, discriminator_B_trainer]
+generators = [generator_A2B, generator_B2A]
+discriminators = [discriminator_A, discriminator_B]
 
 
 
