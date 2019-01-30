b08d2dabe8578fe93d9e4b2eb439f6d6aec90aa8
wazeer zulfikar <wazeer.zulfikar@gmail.com>
2019-01-30 16:35:38 -0500

    Save discriminator as well

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 from utils import *
-from train import generators, trainers
+from train import generators, trainers, discriminators
 import random
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+import time
+from keras.models import load_model
 
-image_size = 64
-batch_size = 16
-n_epochs = 1
-save_step = 20
+image_size = 256
+batch_size = 8
+n_epochs = 201
+save_step = 40
 
-
-trainA = load_dataset('../sample_datasets/horse2zebra/trainA', image_size)
-trainB = load_dataset('../sample_datasets/horse2zebra/trainB', image_size)
-testA = load_dataset('../sample_datasets/horse2zebra/testA', image_size)
-testB = load_dataset('../sample_datasets/horse2zebra/testB', image_size)
+trainA = load_dataset('/horse2zebra/trainA', image_size)
+trainB = load_dataset('/horse2zebra/trainB', image_size)
+testA = load_dataset('/horse2zebra/testA', image_size)
+testB = load_dataset('/horse2zebra/testB', image_size)
 
 print('Number of training samples are {} and {}'.format(len(trainA), len(trainB)))
 print('Number of testing samples are {} and {}'.format(len(testA), len(testB)))
 
 generator_trainer, discriminator_A_trainer, discriminator_B_trainer = trainers
 generator_A2B, generator_B2A = generators
+discriminator_A, discriminator_B = discriminators
 
 print(generator_trainer)
 print(generator_A2B)
@@ -27,24 +29,23 @@ print(generator_A2B)
 def batch_generator(data, batch_size=16):
 	while True:
 		random.shuffle(data)
-		for i in range(0,len(data),batch_size):
+		for i in range(0,len(data)-batch_size,batch_size):
 			batch = data[i:i+batch_size]
 			yield batch
 
-train_A_generator = batch_generator(trainA)
-train_B_generator = batch_generator(trainB)
+train_A_generator = batch_generator(trainA,batch_size)
+train_B_generator = batch_generator(trainB,batch_size)
 
 dummy_labels = np.zeros((batch_size, 1))
 
 for epoch in range(n_epochs):
 	print('Epoch {}'.format(epoch+1))
+	start=time.time()
 
 	for i in range(len(trainA)//batch_size):
 
 		real_A = next(train_A_generator)
 		real_B = next(train_B_generator)
-
-		print(real_A.shape)
 
 		fake_A = generator_A2B.predict(real_B)
 		fake_B = generator_B2A.predict(real_A)
@@ -58,12 +59,12 @@ for epoch in range(n_epochs):
 
 		discriminator_B_trainer.train_on_batch([real_B, fake_B], dummy_labels)
 
-		print('done')
+		# print('Batch done')
 
-		break
+	print('Time taken : '+str(time.time()-start))
 
-	if epoch%save_step==0:
-		generator_A2B.save('saved_models/generator_A2B.h5')
-		generator_B2A.save('saved_models/generator_B2A.h5')
-
-	
+	if epoch>0 and epoch%save_step==0:
+		generator_A2B.save('/saved_models/generator_A2B_{}.h5'.format(epoch))
+		generator_B2A.save('/saved_models/generator_B2A_{}.h5'.format(epoch))
+		discriminator_A.save('/saved_models/discriminator_A_{}.h5'.format(epoch))
+		discriminator_B.save('/saved_models/discriminator_B_{}.h5'.format(epoch))
