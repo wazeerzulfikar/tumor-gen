a68b8f1350d1c94322200ac52fdcbedfa715aea8
wazeer zulfikar <wazeer.zulfikar@gmail.com>
2019-01-31 15:16:07 -0500

    Save generator output at checkpoints

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ from utils import *
 from train import generators, trainers, discriminators
 import random
 import numpy as np
-# import matplotlib.pyplot as plt
 import time
 from keras.models import load_model
 
 image_size = 256
-batch_size = 8
-n_epochs = 201
-save_step = 40
+batch_size = 1
+n_epochs = 301
+save_step = 50
 
-trainA = load_dataset('/horse2zebra/trainA', image_size)
-trainB = load_dataset('/horse2zebra/trainB', image_size)
-testA = load_dataset('/horse2zebra/testA', image_size)
-testB = load_dataset('/horse2zebra/testB', image_size)
+trainA = normalize(load_dataset('/data/horse2zebra/trainA', image_size))
+trainB = normalize(load_dataset('/data/horse2zebra/trainB', image_size))
+testA = load_dataset('/data/horse2zebra/testA', image_size)
+testB = load_dataset('/data/horse2zebra/testB', image_size)
 
 print('Number of training samples are {} and {}'.format(len(trainA), len(trainB)))
 print('Number of testing samples are {} and {}'.format(len(testA), len(testB)))
@@ -23,22 +22,13 @@ generator_trainer, discriminator_A_trainer, discriminator_B_trainer = trainers
 generator_A2B, generator_B2A = generators
 discriminator_A, discriminator_B = discriminators
 
-print(generator_trainer)
-print(generator_A2B)
-
-def batch_generator(data, batch_size=16):
-	while True:
-		random.shuffle(data)
-		for i in range(0,len(data)-batch_size,batch_size):
-			batch = data[i:i+batch_size]
-			yield batch
-
 train_A_generator = batch_generator(trainA,batch_size)
 train_B_generator = batch_generator(trainB,batch_size)
 
 dummy_labels = np.zeros((batch_size, 1))
 
-for epoch in range(n_epochs):
+for epoch in range(0,n_epochs):
+
 	print('Epoch {}'.format(epoch+1))
 	start=time.time()
 
@@ -50,21 +40,20 @@ for epoch in range(n_epochs):
 		fake_A = generator_A2B.predict(real_B)
 		fake_B = generator_B2A.predict(real_A)
 
-		# plt.imshow(fake_A[0])
-		# plt.show()
-
 		generator_trainer.train_on_batch([real_A, real_B], dummy_labels)
 
 		discriminator_A_trainer.train_on_batch([real_A, fake_A], dummy_labels)
 
 		discriminator_B_trainer.train_on_batch([real_B, fake_B], dummy_labels)
 
-		# print('Batch done')
-
 	print('Time taken : '+str(time.time()-start))
 
-	if epoch>0 and epoch%save_step==0:
-		generator_A2B.save('/saved_models/generator_A2B_{}.h5'.format(epoch))
-		generator_B2A.save('/saved_models/generator_B2A_{}.h5'.format(epoch))
-		discriminator_A.save('/saved_models/discriminator_A_{}.h5'.format(epoch))
-		discriminator_B.save('/saved_models/discriminator_B_{}.h5'.format(epoch))
+	if epoch%save_step==0:
+
+		generator_A2B.save('/output/saved_models/generator_A2B_{}_{}.h5'.format(image_size,epoch))
+		generator_B2A.save('/output/saved_models/generator_B2A_{}_{}.h5'.format(image_size,epoch))
+		discriminator_A.save('/output/saved_models/discriminator_A_{}_{}.h5'.format(image_size,epoch))
+		discriminator_B.save('/output/saved_models/discriminator_B_{}_{}.h5'.format(image_size,epoch))
+
+		save_generator_output(testA[np.random.choice(len(testA), size=16, replace=False)], generator_A2B, epoch=epoch, direction='A2B')
+		save_generator_output(testB[np.random.choice(len(testB), size=16, replace=False)], generator_B2A, epoch=epoch, direction='B2A')
--- utils.py
+++ utils.py
@@ -1,8 +1,18 @@
-import cv2
+from PIL import Image
 import glob
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+
+
+def normalize(img):
+	return (img-127.5)/127.5
+
+
+def denormalize(img):
+	return ((img*127.5)+127.5).clip(0,255).astype('uint8')
+
 
 def load_dataset(path, image_size=32):
 	images = []
@@ -10,21 +20,55 @@ def load_dataset(path, image_size=32):
 	files = glob.glob(os.path.join(path,'*.jpg'))
 
 	for f in files:
-		img = cv2.imread(f)
-		img = cv2.cvtColor(cv2.resize(img, (image_size, image_size)), cv2.COLOR_BGR2RGB)
+		img = Image.open(f).convert('RGB')
+		img = img.resize((image_size, image_size))
+		img = np.array(img)
+
+		if np.random.randint(0,2):
+			img = np.fliplr(img)
+
 		images.append(img)
 
 	return np.array(images)
 
-def plot_batch(batch):
 
+def plot_batch(batch):
 	n_elements = len(batch)
-	nrows = int(sqrt(n_elements))
+	nrows = int(math.sqrt(n_elements))
 	ncols = n_elements//nrows
 	fig, axarr = plt.subplots(nrows=nrows, ncols=ncols)
 
-	for i in range(n_rows):
-		for j in range(n_cols):
+	for i in range(nrows):
+		for j in range(ncols):
 			axarr[i,j].imshow(batch[i*ncols+j])
 
 	plt.show()
+
+
+def save_generator_output(batch_x, generator, epoch='', direction='A2B',path='/output/generated'):
+
+	generated_images = generator.predict(normalize(batch_x))
+
+	image_size = batch_x.shape[1]
+	batch_y = denormalize(generated_images)
+
+	output_array = np.vstack([np.hstack((img1, img2)) for img1, img2 in zip(batch_x, batch_y)])
+	output = Image.fromarray(output_batch)
+	output.save(os.path.join(path,'generated_{}_{}_{}.jpg').format(direction, image_size, epoch))
+
+
+def batch_generator(data, batch_size=16):
+	while True:
+		random.shuffle(data)
+		for i in range(0,len(data)-batch_size,batch_size):
+			batch = data[i:i+batch_size]
+			yield batch
+
+
+# import nibabel as nib
+# def read_mri(file_path):
+# 	mri = nib.load(file_path)
+# 	data = mr.get_data()
+# 	mri.uncache()
+# 	return data
+
