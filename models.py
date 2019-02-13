39e8ba617a843838c0269005b9aef9cd62ed722e
wazeer zulfikar <wazeer.zulfikar@gmail.com>
2019-02-13 12:28:16 -0500

    Standardized number of filters for the models

--- models.py
+++ models.py
@@ -1,20 +1,20 @@
 import tensorflow as tf
 from ops import *
 
-def resnet_generator(image, n_res_blocks=9, reuse=False, name='generator'):
+def resnet_generator(image, nf=64, n_res_blocks=9, reuse=False, name='generator'):
 
 	with tf.variable_scope(name, reuse=reuse):
 
-		c0 = conv_block(image, 64, 7, strides=(1,1), name='c0')
-		c1 = conv_block(c0, 128, 3, strides=(2,2), name='c1')
-		c2 = conv_block(c1, 256, 3, strides=(2,2), name='c2')
+		c0 = conv_block(image, nf, 7, strides=(1,1), name='c0')
+		c1 = conv_block(c0, nf*2, 3, strides=(2,2), name='c1')
+		c2 = conv_block(c1, nf*4, 3, strides=(2,2), name='c2')
 		x = c2
 
 		for i in range(n_res_blocks):
-			x = res_block(x, 256, 3, name='res_block_'+str(i))
+			x = res_block(x, nf*4, 3, name='res_block_'+str(i))
 
-		u1 = up_block(x, 128, 3, name='up1')
-		u2 = up_block(u1, 64, 3, name='up2')
+		u1 = up_block(x, nf*2, 3, name='up1')
+		u2 = up_block(u1, nf, 3, name='up2')
 
 		out = tf.nn.tanh(tf.layers.conv2d(u2, 3, 7, strides=(1,1), padding='same'))
 
