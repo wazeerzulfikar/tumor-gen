707ec8b481b35b38444a3900ed0a332f65d154b5
wazeer zulfikar <wazeer.zulfikar@gmail.com>
2019-02-13 17:37:30 -0500

    Divided read and write for tf records experiments

--- preprocess.py
+++ preprocess.py
@@ -6,47 +6,57 @@ import glob
 from tqdm import tqdm
 from utils import *
 
-image_size = 256
+write = True
 
-# dataset_path = '../sample_datasets/horse2zebra/trainA'
-# filenames = glob.glob(os.path.join(dataset_path, '*.jpg'))
+if write:
 
-# def _bytes_feature(value):
-# 	return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+	image_size = 256
 
-# def _int64_feature(value):
-# 	return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+	dataset_path_A = '../sample_datasets/horse2zebra/trainA'
+	dataset_path_B = '../sample_datasets/horse2zebra/trainB'
+	filenames = glob.glob(os.path.join(dataset_path_A, '*.jpg'))+glob.glob(os.path.join(dataset_path_B, '*.jpg'))
+	np.random.shuffle(filenames)
 
-tfrecords_filename = 'horse2zebra.tfrecords'
+	def _bytes_feature(value):
+		return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-# writer = tf.python_io.TFRecordWriter(tfrecords_filename)
+	def _int64_feature(value):
+		return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-# for f in tqdm(filenames):
+	tfrecords_filename = 'horse2zebra.tfrecords'
 
-# 	img = read_image(f, image_size)
-# 	img_raw = img.tostring()
+	writer = tf.python_io.TFRecordWriter(tfrecords_filename)
 
-# 	example = tf.train.Example(features=tf.train.Features(feature={
-# 		'image_size': _int64_feature(image_size),
-# 		'image_raw': _bytes_feature(img_raw)
-# 		}))
+	for f in tqdm(filenames):
 
-# 	writer.write(example.SerializeToString())
+		img = read_image(f, image_size)
+		img_raw = img.tostring()
+		label = 0 if 'trainA' in f else 1
 
-# writer.close()
+		example = tf.train.Example(features=tf.train.Features(feature={
+			'image_size': _int64_feature(image_size),
+			'image_raw': _bytes_feature(img_raw), 
+			'label': _int64_feature(label)
+			}))
 
-recon_images = []
+		writer.write(example.SerializeToString())
 
-record_iterator = tf.python_io.tf_record_iterator(path=tfrecords_filename)
+	writer.close()
 
-for record in record_iterator:
+else:
 
-	example = tf.train.Example()
-	example.ParseFromString(record)
+	# recon_images = []
 
-	img_string = example.features.feature['image_raw'].bytes_list.value[0]
-	img = np.fromstring(img_string, dtype=np.uint8).reshape((image_size, image_size, 3))
+	# record_iterator = tf.python_io.tf_record_iterator(path=tfrecords_filename)
 
-	plt.imshow(img)
-	plt.show()
-	
+	# for record in record_iterator:
+
+	# 	example = tf.train.Example()
+	# 	example.ParseFromString(record)
+
+	# 	img_string = example.features.feature['image_raw'].bytes_list.value[0]
+	# 	img = np.fromstring(img_string, dtype=np.uint8).reshape((image_size, image_size, 3))
+
+	# 	plt.imshow(img)
+	# 	plt.show()
+		
