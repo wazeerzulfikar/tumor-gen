0a3ea01a3e0939e76ed288be1f55f6fd83175fff
wazeer zulfikar <wazeer.zulfikar@gmail.com>
2019-03-07 20:28:26 -0500

    Loss function fixes for cyclegan

--- main_tf_records.py
+++ main_tf_records.py
@@ -17,20 +17,18 @@ print(args.test)
 
 image_size = 256
 batch_size = 1
-channels = 1
+channels = 3
 l1_lambda = 10
 n_epochs = 301
-save_step = 5
+save_step = 10
 continue_train = False
 test = args.test
-checkpoint_dir = '/output/saved_models'
+pool = ImagePool()
 
-tf_records_filename_trainA = '/data/tfrecords/no-tumor.tfrecords'
-tf_records_filename_trainB = '/data/tfrecords/tumor.tfrecords'
+tf_records_filename_trainA = '/data/horse2zebra_trainA.tfrecords'
+tf_records_filename_trainB = '/data/horse2zebra_trainB.tfrecords'
 
-tfconfig = tf.ConfigProto(allow_soft_placement=True)
-tfconfig.gpu_options.allow_growth=True
-sess = tf.Session(config=tfconfig)
+sess = tf.keras.backend.get_session()
 
 use_lsgan = True
 
@@ -39,25 +37,26 @@ if use_lsgan:
 else:
 	gan_criterion = cross_entropy_criterion
 
-adam = tf.keras.optimizers.Adam(lr=2e-4, beta_1=0.5, beta_2=0.999)
+d_optim = tf.train.AdamOptimizer(learning_rate=2e-4, beta1=0.5, beta2=0.999)
+g_optim = tf.train.AdamOptimizer(learning_rate=2e-4, beta1=0.5, beta2=0.999)
 
 img_input_shape = (image_size, image_size, channels)
 
-discriminator_A = create_discriminator(img_input_shape, nf=32, name='discriminator_A')
-discriminator_B = create_discriminator(img_input_shape, nf=32, name='discriminator_B')
+real_A = tf.keras.layers.Input(shape=img_input_shape, name='real_A')
+real_B = tf.keras.layers.Input(shape=img_input_shape, name='real_B')
 
-generator_A2B = resnet_generator(img_input_shape, channels=channels, name='generator_A2B')
-generator_B2A = resnet_generator(img_input_shape, channels=channels, name='generator_B2A')
+discriminator_A = create_discriminator(real_A, nf=32, name='discriminator_A')
+discriminator_B = create_discriminator(real_B, nf=32, name='discriminator_B')
+
+generator_A2B = resnet_generator(real_A, channels=channels, name='generator_A2B')
+generator_B2A = resnet_generator(real_B, channels=channels, name='generator_B2A')
 
 # Discriminator Stuff
 
-discriminator_A.compile(optimizer=adam, loss=gan_criterion)
-discriminator_B.compile(optimizer=adam, loss=gan_criterion)
+# discriminator_A.compile(optimizer=adam, loss=gan_criterion)
+# discriminator_B.compile(optimizer=adam, loss=gan_criterion)
 
 # Generator Stuff
-
-real_A = tf.keras.layers.Input(shape=img_input_shape, name='real_A')
-real_B = tf.keras.layers.Input(shape=img_input_shape, name='real_B')
 
 fake_B = generator_A2B(real_A)
 fake_A = generator_B2A(real_B)
@@ -65,135 +64,125 @@ fake_A = generator_B2A(real_B)
 recon_B = generator_A2B(fake_A)
 recon_A = generator_B2A(fake_B)
 
-frozen_discriminator_A = tf.keras.models.Model(inputs=discriminator_A.inputs, outputs=discriminator_A.outputs)
-frozen_discriminator_A.trainable = False
+pred_A_g = discriminator_A(fake_A)
+pred_B_g = discriminator_B(fake_B)
 
-frozen_discriminator_B = tf.keras.models.Model(inputs=discriminator_B.inputs, outputs=discriminator_B.outputs)
-frozen_discriminator_B.trainable = False
+fake_A_sample = tf.keras.layers.Input(shape=img_input_shape, name='fake_A_sample')
+fake_B_sample = tf.keras.layers.Input(shape=img_input_shape, name='fake_B_sample')
 
-pred_A = frozen_discriminator_A(fake_A)
-pred_B = frozen_discriminator_B(fake_B)
+pred_A_real = discriminator_A(real_A)
+pred_B_real = discriminator_B(real_B)
+
+pred_A_fake = discriminator_A(fake_A_sample)
+pred_B_fake = discriminator_B(fake_B_sample)
 
 reconstructions = tf.keras.layers.Concatenate(axis=0, name='reconstructions')([recon_A, recon_B])
-d_predictions = tf.keras.layers.Concatenate(axis=0, name='d_predictions')([pred_A, pred_B])
+d_predictions = tf.keras.layers.Concatenate(axis=0, name='d_predictions')([pred_A_g, pred_B_g])
+real = tf.keras.layers.Concatenate(axis=0, name='real')([real_A, real_B])
 
-generator_trainer = tf.keras.models.Model(inputs=[real_A, real_B], outputs=[reconstructions, d_predictions])
+g_loss = gan_criterion(tf.ones_like(d_predictions), d_predictions) + 10*cycle_loss(real, reconstructions)
 
-losses = {
-	'reconstructions' : cycle_loss,
-	'd_predictions' : gan_criterion
-}
+d_A_loss = gan_criterion(tf.concat([tf.ones_like(pred_A_real), tf.zeros_like(pred_A_fake)],0), tf.concat([pred_A_real, pred_A_fake],0))
+d_B_loss = gan_criterion(tf.concat([tf.ones_like(pred_B_real), tf.zeros_like(pred_B_fake)],0), tf.concat([pred_B_real, pred_B_fake],0))
+d_loss = 0.5*(d_A_loss+d_B_loss)
 
-loss_weights = {
-	'reconstructions' : 10.0,
-	'd_predictions' : 1.0
-}
+g_grad_a2b = g_optim.compute_gradients(g_loss, generator_A2B.trainable_weights)
+g_grad_b2a = g_optim.compute_gradients(g_loss, generator_B2A.trainable_weights)
+update_g_a2b = g_optim.apply_gradients(g_grad_a2b)
+update_g_b2a = g_optim.apply_gradients(g_grad_b2a)
+d_grad_a = d_optim.compute_gradients(d_loss, discriminator_A.trainable_weights)
+d_grad_b = d_optim.compute_gradients(d_loss, discriminator_B.trainable_weights)
+update_d_a = d_optim.apply_gradients(d_grad_a)
+update_d_b = d_optim.apply_gradients(d_grad_b)
 
-generator_trainer.compile(optimizer=adam, loss=losses, loss_weights=loss_weights)
+def get_internal_updates(model):
+    # get all internal update ops (like moving averages) of a model
+    inbound_nodes = model.inbound_nodes
+    input_tensors = []
+    for ibn in inbound_nodes:
+        input_tensors+= ibn.input_tensors
+    updates = [model.get_updates_for(i) for i in input_tensors]
+    return updates
+
+other_parameter_updates = [get_internal_updates(m) for m in [generator_A2B,generator_B2A,discriminator_A,discriminator_B]]
+
+g_train = [update_g_a2b, update_g_b2a]
+d_train = [update_d_a, update_d_b]
 
 pool = ImagePool()
 
-trainA = tf.data.TFRecordDataset(tf_records_filename_trainA, compression_type='GZIP').map(parse_mri_record, num_parallel_calls=batch_size)
-trainA = trainA.batch(batch_size)
+trainA = tf.data.TFRecordDataset(tf_records_filename_trainA, compression_type='GZIP').map(parse_record, num_parallel_calls=batch_size)
+trainA = trainA.shuffle(100).batch(batch_size)
 trainA_iter = trainA.make_initializable_iterator()
 
-trainB = tf.data.TFRecordDataset(tf_records_filename_trainB, compression_type='GZIP').map(parse_mri_record, num_parallel_calls=batch_size)
-trainB = trainB.batch(batch_size)
+trainB = tf.data.TFRecordDataset(tf_records_filename_trainB, compression_type='GZIP').map(parse_record, num_parallel_calls=batch_size)
+trainB = trainB.shuffle(100).batch(batch_size)
 trainB_iter = trainB.make_initializable_iterator()
 
 trainA_next = trainA_iter.get_next()
 trainB_next = trainB_iter.get_next()
 
-if not test:
+testA = tf.data.TFRecordDataset(tf_records_filename_trainA, compression_type='GZIP').map(parse_record, num_parallel_calls=batch_size)
+testA = testA.shuffle(32).batch(16)
+testA_iter = testA.make_initializable_iterator()
 
-	for epoch in range(n_epochs):
-		start = time.time()
+testB = tf.data.TFRecordDataset(tf_records_filename_trainB, compression_type='GZIP').map(parse_record, num_parallel_calls=batch_size)
+testB = testB.shuffle(32).batch(16)
+testB_iter = testB.make_initializable_iterator()
 
-		sess.run(trainA_iter.initializer)
-		sess.run(trainB_iter.initializer)
-		count = 0
+testA_next = testA_iter.get_next()
+testB_next = testB_iter.get_next()
 
-		while True:
+sess.run(tf.global_variables_initializer())
+learning_phase = tf.keras.backend.learning_phase()
 
-			count +=1
-			print(count)
-			print(time.time()-start)
-
-			try:
-				batch_A = sess.run(trainA_next)
-				batch_B = sess.run(trainB_next)
-			except tf.errors.OutOfRangeError:
-				# Epoch is over
-				break
-
-			fake_A_image = generator_A2B.predict(batch_A)
-			fake_B_image = generator_B2A.predict(batch_B)
-			print('predict', time.time()-start)
-
-			[fake_A_image, fake_B_image] = pool([fake_A_image, fake_B_image])
-
-			label_shape = [batch_size]+list(discriminator_A.output_shape[1:])
-			labels = tf.concat([tf.ones(label_shape), tf.zeros(label_shape)],axis=0)
-
-			d_A_loss = discriminator_A.train_on_batch(x=tf.concat([batch_A, fake_A_image], axis=0), y=labels)
-			d_B_loss = discriminator_B.train_on_batch(x=tf.concat([batch_B, fake_B_image], axis=0), y=labels)
-
-			print('d A loss ', d_A_loss)
-			print('d B loss ', d_B_loss)
-
-			g_loss = generator_trainer.train_on_batch(x=[batch_A, batch_B], 
-				y=[tf.concat([batch_A, batch_B], axis=0), tf.concat([tf.ones(label_shape), tf.ones(label_shape)], axis=0)])
-
-			print('g loss ', g_loss)
-
-		print('Epoch {}, time taken {}'.format(epoch, time.time()-start))
-
-		if epoch%save_step==0:
-			saver.save(sess, os.path.join('/output/saved_models', 'cyclegan.model'), global_step=epoch)
-			# test_idx = np.random.choice(min(len(testA),len(testB)), size=16, replace=False)
-			# test_batch = norm(np.concatenate((testA[test_idx],testB[test_idx]), axis=3))
-			# fake_A_image, fake_B_image = sess.run(
-			# 	[fake_A, fake_B],
-			# 	feed_dict={real_data: test_batch})
-			# save_generator_output(test_batch[:,:,:,:channels], fake_B_image, epoch=epoch, direction='A2B')
-			# save_generator_output(test_batch[:,:,:,channels:], fake_A_image, epoch=epoch, direction='B2A')
-
-			# save_generator_output(batch_A_B[:,:,:,:channels], fake_B_image, epoch=epoch, direction='A2B')
-			# save_generator_output(batch_A_B[:,:,:,channels:], fake_A_image, epoch=epoch, direction='B2A')
-
-else:
-
-	ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
-	ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
-
-	print('model at {} loaded'.format(ckpt_name))
-
-	saver.restore(sess, os.path.join(checkpoint_dir, ckpt_name))
-
-	test_A = tf.placeholder(tf.float32, [None, image_size, image_size, channels], name='test_A')
-	test_B = tf.placeholder(tf.float32, [None, image_size, image_size, channels], name='test_B')
-
-	output_test_B = resnet_generator(test_A, channels=channels, reuse=True, name='generator_A2B')
-	output_test_A = resnet_generator(test_B, channels=channels, reuse=True, name='generator_B2A')
+for epoch in range(n_epochs):
+	start = time.time()
 
 	sess.run(trainA_iter.initializer)
 	sess.run(trainB_iter.initializer)
 
-	for i in range(5):
-		batch_A = sess.run(trainA_next)
-		batch_B = sess.run(trainB_next)
+	sess.run(testA_iter.initializer)
+	sess.run(testB_iter.initializer)
 
-		fake_B_image = sess.run(output_test_B, feed_dict={test_A: batch_A})
-		fake_A_image = sess.run(output_test_A, feed_dict={test_B: batch_B})
+	while True:
+		try:
+			batch_A = sess.run(trainA_next)
+			batch_B = sess.run(trainB_next)
 
-		save_mri_image(batch_A[0], '/output/generated/{}_real_A.nii.gz'.format(i))
-		save_mri_image(fake_B_image[0], '/output/generated/{}_fake_B_image.nii.gz'.format(i))
+		except tf.errors.OutOfRangeError:
+			# Epoch is over
+			break
 
-		save_mri_image(batch_B[0], '/output/generated/{}_real_B.nii.gz'.format(i))
-		save_mri_image(fake_A_image[0], '/output/generated/{}_fake_A_image.nii.gz'.format(i))
+		fake_A_image, fake_B_image, _ = sess.run([fake_A, fake_B, g_train], 
+			feed_dict={real_A: batch_A, real_B: batch_B, learning_phase: True})
 
-		print(i,' Done')
+		fake_A_image, fake_B_image = pool([fake_A_image, fake_B_image])
 
+		_, loss_A, loss_B = sess.run([d_train, d_A_loss, d_B_loss], 
+			feed_dict={real_A: batch_A, real_B: batch_B, fake_A_sample: fake_A_image,
+			fake_B_sample: fake_B_image, learning_phase: True})
+
+
+	print('Epoch {}, time taken {}'.format(epoch, time.time()-start))
+
+	batch_testA = sess.run(testA_next)
+	batch_testB = sess.run(testB_next)
+
+	fake_A_image, fake_B_image = sess.run([fake_A, fake_B], 
+		feed_dict={real_A: batch_testA, real_B: batch_testB, learning_phase: False})
+
+	save_generator_output(batch_testB, fake_A_image, direction='A2B')
+	save_generator_output(batch_testA, fake_B_image, direction='B2A')
+
+
+	if epoch%save_step==0:
+		generator_A2B.save('/output/saved_models/generator_A2B.h5')
+		generator_B2A.save('/output/saved_models/generator_B2A.h5')
+		discriminator_A.save('/output/saved_models/discriminator_A.h5')
+		discriminator_B.save('/output/saved_models/discriminator_B.h5')
+		save_generator_output(batch_testB, fake_A_image,epoch=epoch, direction='A2B')
+		save_generator_output(batch_testA, fake_B_image, epoch=epoch, direction='B2A')
 
 
 
