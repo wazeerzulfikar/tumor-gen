5db6a5d3bd48885f30c6f1c98567da0e125998b6
wazeer zulfikar <wazeer.zulfikar@gmail.com>
2019-03-13 15:50:00 -0400

    Added self attention layer, and fix for relu while loading

--- ops.py
+++ ops.py
@@ -1,8 +1,10 @@
 import tensorflow as tf 
+import tensorflow.keras.backend as K
+# tf.enable_eager_execution()
 
 class InstanceNormalization(tf.keras.layers.Layer):
-	def __init__(self):
-		super(InstanceNormalization, self).__init__()
+	def __init__(self, **kwargs):
+		super(InstanceNormalization, self).__init__(**kwargs)
 
 	def build(self, input_shape):
 		depth = int(input_shape[-1])
@@ -32,13 +34,13 @@ def conv_block(inputs, filters, kernel_size=4, strides=(2,2), padding='same',
 			x = InstanceNormalization()(x)
 		else:
 			x = tf.keras.layers.BatchNormalization(
-				momemtum=0.9,
+				momentum=0.9,
 				epsilon=1e-5
 				)(x)
 
 	if has_activation_layer:
 		if use_leaky_relu:
-			x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
+			x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
 		else:
 			x = tf.keras.layers.ReLU()(x)
 
@@ -47,15 +49,15 @@ def conv_block(inputs, filters, kernel_size=4, strides=(2,2), padding='same',
 
 def res_block(inputs, filters=32, use_dropout=False):
 
-	y = conv_block(inputs, filters, kernel_size=3, strides=(1,1))
+	y = conv_block(inputs, filters, kernel_size=3, strides=(1,1), use_leaky_relu=False)
 	if use_dropout:
 		y = tf.keras.layers.Dropout(0.5)(y)
 	y = conv_block(y, filters, kernel_size=3, strides=(1,1), has_activation_layer=False)
 
-	return tf.keras.layers.Add()([inputs, y])
+	return tf.keras.layers.Add()([y, inputs])
 
 
-def up_block(inputs, filters, kernel_size=3, strides=2, use_conv2d_transpose=True, use_instance_norm=True):
+def up_block(inputs, filters, kernel_size=4, strides=2, use_conv2d_transpose=True, use_instance_norm=True):
 
 	x = tf.keras.layers.Conv2DTranspose(
 		filters=filters,
@@ -69,12 +71,72 @@ def up_block(inputs, filters, kernel_size=3, strides=2, use_conv2d_transpose=Tru
 			x = InstanceNormalization()(x)
 	else:
 		x = tf.keras.layers.BatchNormalization(
-			momemtum=0.9,
+			momentum=0.9,
 			epsilon=1e-5
 			)(x)
 
 	x = tf.keras.layers.ReLU()(x)
 
 	return x
+
+
+
+def hw_flatten(x):
+    # Input shape x: [BATCH, HEIGHT, WIDTH, CHANNELS]
+    # flat the feature volume across the width and height dimensions 
+    x_shape = tf.shape(x)
+    return tf.reshape(x, [x_shape[0], -1, x_shape[-1]]) # return [BATCH, W*H, CHANNELS]
+
+
+class SelfAttention(tf.keras.Model):
+  def __init__(self, number_of_filters):
+    super(SelfAttention, self).__init__()
+    
+    self.f = tf.keras.layers.Conv2D(number_of_filters//8, 1,
+                                     strides=1, padding='SAME', name="f_x",
+                                     activation=None)
+    
+    self.g = tf.keras.layers.Conv2D(number_of_filters//8, 1,
+                                     strides=1, padding='SAME', name="g_x",
+                                     activation=None)
+    
+    self.h = tf.keras.layers.Conv2D(number_of_filters, 1,
+                                     strides=1, padding='SAME', name="h_x",
+                                     activation=None)
+    
+    self.gamma = tf.Variable(0., dtype=tf.float32, trainable=True, name="gamma")
+    
+  def call(self, x):
+
+    f = self.f(x)
+    g = self.g(x)
+    h = self.h(x)
+    
+    f_flatten = hw_flatten(f)
+    g_flatten = hw_flatten(g)
+    h_flatten = hw_flatten(h)
+    
+    s = tf.matmul(g_flatten, f_flatten, transpose_b=True) # [B,N,C] * [B, N, C] = [B, N, N]
+
+    b = tf.nn.softmax(s, axis=-1)
+    o = tf.matmul(b, h_flatten)
+
+    y = self.gamma * tf.reshape(o, tf.shape(x)) + x
+
+    return y
+
+class ReLU(tf.keras.layers.ReLU):
+
+	def __init__(self, max_value=None, negative_slope=0, threshold=0, **kwargs):
+
+		if type(max_value) is dict:
+			max_value = max_value['value']
+		if type(negative_slope) is dict:
+			negative_slope = negative_slope['value']
+		if type(threshold) is dict:
+			threshold = threshold['value']
+
+		super(ReLU, self).__init__(max_value=None, negative_slope=0, threshold=0, **kwargs)
+
 
 
